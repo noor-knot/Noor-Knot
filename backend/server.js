4c6c1bc72a72659c772769c21b85154c08cb70f0
vidhigit07 <gvidhi0710@gmail.com>
2026-06-25 00:05:09 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,8 @@ app.post('/noor', async (req, res) => {
     ];
 
     const response = await fetch(
-      `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${process.env.GEMINI_KEY}`,
+      
+       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite-preview-06-17:generateContent?key=${process.env.GEMINI_KEY}`,
       {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
